26fd624f3458c81749d371df682ab3f256a1c681
Kari <kari.lonczak@gmail.com>
2015-10-16 12:50:42 -0700

    Created sessions controller to handle signing in users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_create_from_omniauth(auth_hash)
+    session[:user_id] = @user.id
+
+    redirect_to home_path
+  end
+
+  def oauth_failure
+    flash[:error] = "There was an error with signing in with Spotify. Please try again."
+    
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+end
