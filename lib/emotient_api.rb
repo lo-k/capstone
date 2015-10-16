7d8a58ba928b8c0067635ecb8c33866413d83901
Kari <kari.lonczak@gmail.com>
2015-10-16 12:54:02 -0700

    Wrote method to retrieve an emotient video's analysis csv file and save its results in an emotions hash. This will likely need to be tweaked later to be more stable. It currently is assuming that the indices will not change in the csv results file.

--- lib/emotient_api.rb
+++ lib/emotient_api.rb
@@ -1,14 +1,40 @@
 class EmotientApi
-  EMO_URI = "https://api.emotient.com/v1/upload"
+  EMO_URI = "https://api.emotient.com/v1/"
+  INTERVAL = "summary"
+  REPORT = "standard"
+  GENDER = "combined"
 
   def upload_video(video)
-    response = HTTMultiParty.post(EMO_URI,
+    response = HTTMultiParty.post(EMO_URI + "upload",
       :query => { :file => video },
-      :headers => { 'Authorization' => "a4687ea8-a1b0-4c24-88a7-3220f865a27a" , 'Content-Type' => 'multipart/form-data'})
+      :headers => { 'Authorization' => "a4687ea8-a1b0-4c24-88a7-3220f865a27a" , 
+        'Content-Type' => 'multipart/form-data'})
 
     id = response.parsed_response["id"] 
 
     return id
+  end
+
+  def analyze_video(video_id)
+    response = HTTMultiParty.get(EMO_URI + "analytics/" + video_id + "/aggregate?interval=" + 
+      INTERVAL + "&report=" + REPORT + "&gender=" + GENDER,
+      :headers => { 'Authorization' => "a4687ea8-a1b0-4c24-88a7-3220f865a27a" })
+
+    emotions_hash = {
+      sentiment: response.parsed_response[1][11],
+      positive: response.parsed_response[1][12],
+      negative: response.parsed_response[1][13],
+      neutral: response.parsed_response[1][14],
+      anger: response.parsed_response[1][15],
+      contempt: response.parsed_response[1][16],
+      disgust: response.parsed_response[1][17],
+      fear: response.parsed_response[1][18],
+      joy: response.parsed_response[1][19],
+      sadness: response.parsed_response[1][20],
+      surprise: response.parsed_response[1][21]
+    }
+
+    return emotions_hash
   end
 
 end
