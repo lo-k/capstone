0de895a36dcd6ce9cf8c0bf62ad07a98dbaa3254
Kari <kari.lonczak@gmail.com>
2015-10-21 14:21:50 -0700

    Updated files with pg db instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151013213432) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "spotify_id"
