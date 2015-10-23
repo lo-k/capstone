c0df489028c74c3666dc2194cdaf3a55a247fe11
Kari <kari.lonczak@gmail.com>
2015-10-23 15:41:23 -0700

    Added code for video recorder with RecordRTC. Commented out other video example, which seemed more complicated. Added upload video and take video glyphicons to page, which will trigger which submission method the user wants to do. Added jquery listener on the upload video glyphicon to open the file input field.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,13 @@ $(document).ready( function() {
       "audio/x-ms-wmv",     // .wmv
     ]
 
+  // Upload Video Button Triggers Input Select
+
+  $('#video-upload').click(function() {
+    var fileInput = $('input:file');
+    fileInput.click();
+  });
+
   $('input:file').change(function() {
 
       var empty = false;
@@ -67,5 +74,161 @@ $(document).ready( function() {
       }
 
   });
+
+  // ---------------------- WEBCAM VIDEO ----------------------
+
+  // config
+  var videoWidth = 640;
+  var videoHeight = 480;
+
+  // setup
+  var stream;
+  var video_recorder = null;
+  var recording = false;
+  var playing = false;
+  var formData = null;
+
+  // set the video options
+  var videoOptions = {
+    type: "video",
+    video: {
+      width: videoWidth,
+      height: videoHeight
+    },
+    canvas: {
+      width: videoWidth,
+      height: videoHeight
+    }
+  };
+
+  // record the video
+  var recordVideo = function() { 
+    navigator.getUserMedia({audio: false, video: { mandatory: {}, optional: []}}, function(pStream) {
+
+      stream = pStream;
+      // setup video
+      video = $("video.recorder")[0];
+
+      video.src = window.URL.createObjectURL(stream);
+      video.width = videoWidth;
+      video.height = videoHeight;
+
+      // init recorder
+      video_recorder = RecordRTC(stream, videoOptions);
+
+      // update UI
+      $("#record_button").show();
+    }, function(){})
+  };
+
+  // start recording
+  var startRecording = function() {
+    video_recorder.startRecording();
+
+    // update the UI
+    $("#play_button").hide();
+    $("#upload_button").hide();
+    $("video.recorder").show();
+    $("#video-player").remove();
+    $("#record_button").text("Stop recording");
+
+    // toggle boolean
+    recording = true;
+  }
+
+  // stop recording
+  var stopRecording = function() {
+    video_recorder.stopRecording();
+
+    // set form data
+    formData = new FormData();
+    var video_blob = video_recorder.getBlob();
+    formData.append("video", video_blob);
+
+    // add player
+    var video_player = document.createElement("video");
+    video_player.id = "video-player";
+    video_player.src = URL.createObjectURL(video_blob);
+    video_player.controls = "controls";
+    $("#players").append(video_player);
+
+    // update UI
+    $("video.recorder").hide();
+    $("#play_button").show();
+    $("#upload_button").show();
+    $("#record_button").text("Start recording");
+
+    // toggle boolean
+    recording = false;
+  }
+
+  // handle recording
+  $("#record_button").click(function(){
+    if (recording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  });
+
+  // stop playback
+  var stopPlayback = function() {
+    // controlling
+    video = $("#video-player")[0];
+    video.pause();
+    video.currentTime = 0;
+
+    // update ui
+    $("#play_button").text("Play");
+
+    // toggle boolean
+    playing = false;
+  }
+
+  // start playback
+  var startPlayback = function() {
+    video = $("#video-player")[0];
+    video.play();
+    $("#video-player").bind("ended", stopPlayback);
+
+    // Update UI
+    $("#play_button").text("Stop");
+
+    // toggle boolean
+    playing = true;
+  }
+
+  $('#record-video').click(function() {
+    $('video.recorder').show();
+    recordVideo();
+  })
+
+  // handle playback
+  $("#play_button").click(function(){
+    if (playing) {
+      stopPlayback();
+    } else {
+      startPlayback();
+    }
+  });
+
+  // Upload button
+  $("#upload_button").click(function(){
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == 200) {
+        window.location.href = "/video/"+request.responseText;
+      }
+    };
+
+    request.open('POST', "/upload");
+    request.send(formData);
+  });
+
+  $('#turn-off').click(function() {
+    stream.stop();
+  })
+
 
 })
